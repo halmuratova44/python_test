4f21c9f980fcda99f1bd76e37772191417e900da
ismasele <83342.a@gmail.com>
2024-11-11 12:52:09 +0600

    аше сообщение коммита

--- booking/booking/urls.py
+++ booking/booking/urls.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include
 from flights import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include('airline.urls')),  # Заменено на airline
-    path('flights/', include('flights.urls'))
-=======
-from django.urls import include, path
-
-urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('airline.urls')),  # Подключаем URL из приложения airline
->>>>>>> origin/master
+    path('', include('airline.urls')),
+    path('flights/', include('flights.urls'))
 ]
+
+
+
